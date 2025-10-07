f9980290f6330de0faefc297acfe14f3573be240
GeorgeShani <shanidze.geo2004@gmail.com>
2025-10-07 10:54:25 +0400

    add export

--- ChakruloApp/src/services/internal/index.ts
+++ ChakruloApp/src/services/internal/index.ts
@@ -3,3 +3,6 @@ export * from "./supabase/conversation";
 export * from "./supabase/message";
 export * from "./supabase/questions";
 export * from "./supabase/storage";
+
+export * from "./buildGeminiPrompt";
+export * from "./formatRecommendations";
