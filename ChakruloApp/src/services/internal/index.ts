bab1256ce65bb10d2cb0113e9585a638819006f6
GeorgeShani <shanidze.geo2004@gmail.com>
2025-10-07 20:55:13 +0400

    move functions from services to utils

--- ChakruloApp/src/services/internal/index.ts
+++ ChakruloApp/src/services/internal/index.ts
@@ -2,6 +2,3 @@ export * from "./supabase/user";
 export * from "./supabase/questions";
 export * from "./supabase/storage";
 export * from "./supabase/submission";
-
-export * from "./buildGeminiPrompt";
-export * from "./formatRecommendations";
