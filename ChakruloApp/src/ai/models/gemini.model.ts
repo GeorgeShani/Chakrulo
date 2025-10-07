d2d997ba99662482803b9d02696268824f04b847
GeorgeShani <shanidze.geo2004@gmail.com>
2025-10-07 10:53:52 +0400

    modify import

--- ChakruloApp/src/ai/models/gemini.model.ts
+++ ChakruloApp/src/ai/models/gemini.model.ts
@@ -1,4 +1,4 @@
-import { genAI } from "../config/gemini.config";
+import { genAI } from "@/ai/config/gemini.config";
 
 export const getGeminiModel = (modelName: string = "gemini-2.0-flash") => {
   return genAI.getGenerativeModel({ model: modelName });
